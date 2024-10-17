9aee99bcbc724e50751837a971b902c104887f68
brenocrepaldi <brenogaia2004@gmail.com>
2024-10-17 15:22:26 -0300

    style: menu button

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
 import { LoginPage } from './pages/login/index';
 import { DashboardPage } from './pages/dashboard/index';
 
@@ -6,6 +11,7 @@ export function App() {
 	return (
 		<Router>
 			<Routes>
+				<Route path="/" element={<Navigate to="/login" />} />
 				<Route path="/login" element={<LoginPage />} />
 				<Route path="/dashboard" element={<DashboardPage />} />
 			</Routes>
--- src/pages/dashboard/dashboard-components/menu-bar/index.tsx
+++ src/pages/dashboard/dashboard-components/menu-bar/index.tsx
@@ -16,13 +16,13 @@ export function MenuBar({ toggleMenubar, isMenubarOpen }: MenuBarProps) {
 	return (
 		<div
 			className={`bg-zinc-800 h-screen py-5 px-3 flex flex-col overflow-hidden shadow-shape ${
-				isMenubarOpen ? 'w-64 items-start' : 'w-20 items-center'
+				isMenubarOpen ? 'w-64 items-center' : 'w-20 items-center'
 			} transition-all duration-300`}
 			style={{ position: 'fixed', left: 0, top: 0, bottom: 0 }}
 		>
 			<button
 				onClick={toggleMenubar}
-				className={`mb-2 w-fit text-zinc-300 hover:text-white hover:bg-zinc-700 p-3 rounded-full flex gap-2 items-center ${
+				className={`mb-2 w-full rounded-md text-zinc-300 hover:text-white hover:bg-zinc-700 p-3 flex gap-2 items-center ${
 					isMenubarOpen ? 'justify-start' : 'justify-center'
 				}`}
 			>
