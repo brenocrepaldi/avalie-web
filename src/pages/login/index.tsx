6ea215530b0247cdb81ede21ce514fb54fa546bd
brenocrepaldi <brenogaia2004@gmail.com>
2024-10-25 11:12:55 -0300

    refactor: add login password visibility

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,4 @@
+import { EyeIcon, EyeOff } from 'lucide-react';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components/button';
@@ -11,6 +12,7 @@ export function LoginPage() {
 		useState(false);
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [showPassword, setShowPassword] = useState(false);
 
 	useEffect(() => {
 		const token = localStorage.getItem('accessToken');
@@ -21,6 +23,11 @@ export function LoginPage() {
 
 	const handleForgotPasswordModal = () => {
 		setIsForgotPasswordModalOpen(!isForgotPasswordModalOpen);
+	};
+
+	const handlePasswordVisibility = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		showPassword ? setShowPassword(false) : setShowPassword(true);
 	};
 
 	function UserLogIn(event: React.FormEvent<HTMLFormElement>) {
@@ -36,7 +43,7 @@ export function LoginPage() {
 
 	return (
 		<div className="bg-zinc-900 h-screen flex items-center justify-center">
-			<div className="bg-zinc-800 w-[640px] h-auto rounded-xl py-7 px-6 shadow-shape flex flex-col justify-center gap-10">
+			<div className="bg-zinc-800 w-[700px] h-auto rounded-xl py-7 px-12 shadow-shape flex flex-col justify-center gap-10">
 				<div className="w-full text-center">
 					<h2 className="text-3xl font-semibold">Login</h2>
 				</div>
@@ -46,40 +53,53 @@ export function LoginPage() {
 						Preencha seus dados para realizar o login.
 					</p>
 					<div className="space-y-2">
-						<div className="h-12 px-4 bg-zinc-900 border-zinc-800 rounded-lg flex items-center gap-2">
+						<div className="h-12 px-4 bg-zinc-900 border-zinc-700 border-2 rounded-lg flex items-center gap-2">
 							<input
 								type="email"
 								name="email"
 								placeholder="Digite seu e-mail"
-								className="bg-transparent text-base placeholder-zinc-400 outline-none flex-1"
 								value={email}
-								onChange={(e) => setEmail(e.target.value)} // Atualiza o estado do email
+								onChange={(e) => setEmail(e.target.value)}
+								className="bg-transparent text-base placeholder-zinc-400 outline-none flex-1"
+								required
 							/>
 						</div>
-
-						<div className="h-12 px-4 bg-zinc-900 border-zinc-800 rounded-lg flex items-center gap-2">
+						<div className="h-12 px-4 bg-zinc-900 border-zinc-700 border-2 rounded-lg flex items-center gap-2">
 							<input
-								type="password"
+								type={showPassword ? 'text' : 'password'}
 								name="password"
 								placeholder="Digite sua senha"
-								className="bg-transparent text-base placeholder-zinc-400 outline-none flex-1"
 								value={password}
-								onChange={(e) => setPassword(e.target.value)} // Atualiza o estado da senha
+								onChange={(e) => setPassword(e.target.value)}
+								className="bg-transparent text-base placeholder-zinc-400 outline-none flex-1"
+								required
 							/>
+							<button
+								onClick={(e) => {
+									handlePasswordVisibility(e);
+								}}
+							>
+								{showPassword ? (
+									<EyeOff className="text-zinc-400" />
+								) : (
+									<EyeIcon className="text-zinc-400" />
+								)}
+							</button>
 						</div>
 					</div>
-					<Button type="submit" variant="primary" size="full">
-						Entrar
-					</Button>
-
-					<div className="w-full flex justify-center">
-						<button
-							type="button"
-							className="text-sm text-zinc-300 underline hover:text-zinc-200"
-							onClick={handleForgotPasswordModal}
-						>
-							Esqueci minha senha
-						</button>
+					<div className="space-y-2">
+						<Button type="submit" variant="primary" size="full">
+							Entrar
+						</Button>
+						<div className="w-full flex justify-center">
+							<button
+								type="button"
+								className="text-sm text-zinc-300 underline hover:text-zinc-200"
+								onClick={handleForgotPasswordModal}
+							>
+								Esqueci minha senha
+							</button>
+						</div>
 					</div>
 				</form>
 			</div>
